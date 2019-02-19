4cc5c0badb16cc0acb1f1c141f7a51828ca5996a
Abdurrehman <abdulrehman9556@gmail.com>
2019-02-20 02:19:56 +0500

    about us page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,8 @@ const routes: Routes = [
   { path: 'add-medicine', loadChildren: './add-medicine/add-medicine.module#AddMedicinePageModule' },
   { path: 'medicine-list', loadChildren: './medicine-list/medicine-list.module#MedicineListPageModule' },
   { path: 'location', loadChildren: './location/location.module#LocationPageModule' },
-  { path: 'medical-med-add', loadChildren: './medical-med-add/medical-med-add.module#MedicalMedAddPageModule' }
+  { path: 'medical-med-add', loadChildren: './medical-med-add/medical-med-add.module#MedicalMedAddPageModule' },  { path: 'about-us', loadChildren: './about-us/about-us.module#AboutUsPageModule' }
+
 ];
 
 
