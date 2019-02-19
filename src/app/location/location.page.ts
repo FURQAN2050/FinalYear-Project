d1c9e5bf8b485ccfe42225edb7a00aac37e3bfa2
Unknown <furqdjrian@gmail.com>
2019-02-19 15:18:58 +0500

    add location succesfully

--- src/app/location/location.page.ts
+++ src/app/location/location.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit,ViewChild } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 // import { google } from '@google/maps';
 declare var google : any;
 @Component({
@@ -9,13 +10,53 @@ declare var google : any;
 export class LocationPage implements OnInit {
   @ViewChild("map") mapElement;
   map :any;
-  constructor() { }
+  start :any;
+  end:any;
+  lat:any ;
+  long:any;
+  currentlocation:any;
+  constructor(
+    public geolocation:Geolocation
+  ) { }
+  ionViewDidLoad() {
+    
+  }
+  calculateAndDisplayRoute() {
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 7,
+      center: this.currentlocation
+    });
+    directionsDisplay.setMap(map);
 
+     directionsService.route({
+        origin: this.currentlocation,
+        destination: this.end,
+        travelMode: 'DRIVING'
+      }, function(response, status) {
+        if (status === 'OK') {
+          directionsDisplay.setDirections(response);
+        } else {
+          window.alert('Directions request failed due to ' + status);
+        }
+      });
+}
   ngOnInit() {
-    this.initMap()
+    this.initMap();
+    this.geolocation.getCurrentPosition()
+    .then(pos=>{
+      this.lat=pos.coords.latitude;
+      this.long=pos.coords.longitude;
+      this.currentlocation=new google.maps.LatLng(this.lat,this.long);
+      console.log('current postion',this.lat,this.long);
+    }).catch(err=>{
+      console.log(err);
+    })
+    console.log('ionViewDidLoad LocationPage');
   }
   initMap(){
-    let coords=new google.maps.LatLng(45,100);
+    let coords=new google.maps.LatLng(-34.397,150.644);
     let mapOptions : google.maps.MapOptions={
     center:coords,zoom:14,mapTypeId:google.maps.MapTypeId.ROADMAP 
     }
