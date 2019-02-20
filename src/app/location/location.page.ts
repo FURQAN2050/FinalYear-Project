ad6bff6d531900424a26b3383abadc6c3ad3e31f
Unknown <furqdjrian@gmail.com>
2019-02-20 15:18:28 +0500

    some work

--- src/app/location/location.page.ts
+++ src/app/location/location.page.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit,ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 // import { google } from '@google/maps';
-// declare var google : any;
+import { google } from '@google/maps';
+declare var google: any;
 @Component({
   selector: 'app-location',
   templateUrl: './location.page.html',
@@ -9,17 +10,41 @@ import { Geolocation } from '@ionic-native/geolocation/ngx';
 })
 export class LocationPage implements OnInit {
   @ViewChild("map") mapElement;
-  map :any;
-  start :any;
-  end:any;
-  lat:any ;
-  long:any;
-  currentlocation:any;
+  map: any;
+  start: any;
+  end: any;
+  lat: any;
+  long: any;
+  currentlocation: any;
+  findPosition:boolean;
   constructor(
-    public geolocation:Geolocation
-  ) { }
-  ionViewDidLoad() {
-    
+    public geolocation: Geolocation
+  ) { 
+    this.findPosition=false;
+  }
+  ionViewDidLoad() { }
+
+  ngOnInit() {
+    this.initMap();
+    this.geolocation.getCurrentPosition()
+      .then(pos => {
+        this.lat = pos.coords.latitude;
+        this.long = pos.coords.longitude;
+        this.currentlocation = new google.maps.LatLng(this.lat, this.long);
+        console.log('current postion', this.lat, this.long);
+      }).catch(err => {
+        console.log(err);
+      })
+  }
+
+  initMap() {
+    let coords = new google.maps.LatLng(-34.397, 150.644);
+    let mapOptions: google.maps.MapOptions = {
+      center: coords, zoom: 14, mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions)
+    let marker: google.maps.Marker = new google.maps.Marker({
+    })
   }
   calculateAndDisplayRoute() {
     var directionsService = new google.maps.DirectionsService;
@@ -30,39 +55,24 @@ export class LocationPage implements OnInit {
     });
     directionsDisplay.setMap(map);
 
-     directionsService.route({
-        origin: this.currentlocation,
-        destination: this.end,
-        travelMode: 'DRIVING'
-      }, function(response, status) {
-        if (status === 'OK') {
-          directionsDisplay.setDirections(response);
-        } else {
-          window.alert('Directions request failed due to ' + status);
-        }
-      });
-}
-  ngOnInit() {
-    this.initMap();
-    this.geolocation.getCurrentPosition()
-    .then(pos=>{
-      this.lat=pos.coords.latitude;
-      this.long=pos.coords.longitude;
-      this.currentlocation=new google.maps.LatLng(this.lat,this.long);
-      console.log('current postion',this.lat,this.long);
-    }).catch(err=>{
-      console.log(err);
-    })
-    console.log('ionViewDidLoad LocationPage');
+    directionsService.route({
+      origin: this.currentlocation,
+      destination: this.end,
+      travelMode: 'DRIVING'
+    }, function (response, status) {
+      if (status === 'OK') {
+        directionsDisplay.setDirections(response);
+      } else {
+        alert('Directions request failed due to ' + status);
+      }
+    });
   }
-  initMap(){
-    let coords=new google.maps.LatLng(-34.397,150.644);
-    let mapOptions : google.maps.MapOptions={
-    center:coords,zoom:14,mapTypeId:google.maps.MapTypeId.ROADMAP 
-    }
-    this.map=new google.maps.Map(this.mapElement.nativeElement,mapOptions)
-    let marker :google.maps.Marker=new google.maps.Marker({
-    }) 
-    
-    }
+  
+  find(){
+    if(this.findPosition)
+    this.findPosition=false;
+    else
+    this.findPosition=true;
+
+  }
 }
