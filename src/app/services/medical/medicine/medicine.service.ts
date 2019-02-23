923b7ac435c3623f90ec8b433e6d753e232d7327
Muhammad Furqan <furqdjrain@gmail.com>
2019-02-23 15:49:14 +0500

    medical Store Mdecine

---
+++ src/app/services/medical/medicine/medicine.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { BaseUrlService } from '../../baseUrl/base-url.service'
+import { map } from 'rxjs/operators';
+@Injectable({
+  providedIn: 'root'
+})
+export class MedicineService {
+
+  constructor(
+    private http: Http,
+    private BaseUrlService: BaseUrlService
+  ) { }
+  updateMedicine(medicine) {
+    console.log(medicine);
+    var api = this.BaseUrlService.getBase_Url() + '/api/medMidicine';
+    return this.http.put(api, medicine)
+      .pipe(map((response: Response) => response.json()))
+  }
+  getMedicine(medicineId){
+    console.log(medicineId)
+    var api = this.BaseUrlService.getBase_Url() + '/api/medMidicine/'+medicineId;
+    return this.http.get(api)
+    .pipe(map((response: Response) => response.json()))
+  }
+  getAllMedicine(){
+    var api = this.BaseUrlService.getBase_Url() + '/api/medMidicine';
+    return this.http.get(api)
+    .pipe(map((response: Response) => response.json()))
+
+  }
+}
