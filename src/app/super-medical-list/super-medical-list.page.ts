074cfafd5f009883e86c1e727eea528859b23141
MUHAMMAD <FURQAN>
2019-03-18 13:49:37 +0500

    add the mecical Store list

--- src/app/super-medical-list/super-medical-list.page.ts
+++ src/app/super-medical-list/super-medical-list.page.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import {MedicalStoreService} from '../services/medical/MedicalStore/medical-store.service'
 @Component({
   selector: 'app-super-medical-list',
   templateUrl: './super-medical-list.page.html',
   styleUrls: ['./super-medical-list.page.scss'],
 })
 export class SuperMedicalListPage implements OnInit {
+  medicalStores: any;
 
-  constructor() { }
+  constructor(public MedicalStoreService:MedicalStoreService) { }
 
   ngOnInit() {
+  }
+  ionViewWillEnter(){
+    this.getAllMedicalStores();
+  }
+  getAllMedicalStores(){
+    this.MedicalStoreService.getAllMedicalStore().subscribe(medicalStores=>{
+      this.medicalStores=medicalStores;
+      console.log(this.medicalStores);
+    })
   }
 
 }
